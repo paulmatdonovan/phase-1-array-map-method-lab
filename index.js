a92c64f545811312655d317631e1629093a3369d
Paul Donovan <paul.m.donovan@outlook.com>
2024-03-12 17:58:14 +1100

    solutionadded

--- index.js
+++ index.js
@@ -11,6 +11,64 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// my attempts:
+
+// const titleCased = (someArray) => {
+//   for (let i = 0; i < someArray.length; i++) {
+//     const mySentence = someArray[i];
+//     const words = mySentence.split(" ");
+
+//     words.map((word) => {
+//       return word[0].toUpperCase() + word.substring(1);
+//     }).join(" ");
+//   }
+
+//   return titleCased(someArray);
+// }
+
+
+//   return tutorials
+// }
+
+// function titleCased(array) {
+//   for (sentence in array) {
+
+//     const words = sentence.split(" ");
+
+//     for (let i = 0; i < words.length; i++) {
+//       words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+//     }
+//     return words.join(" ");
+
+//   }
+//   return titleCased();
+// }
+// console.log(titleCased(tutorials));
+
+
+
+// Academy xit
+
+
+// const titleCased = () => {
+//   return tutorials
+// }
+// return tutorials.map((line) => {
+//   const tokens = line.split(" ");
+//   const capitalizedTokens = tokens.map(
+//     (token) => token.charAt(0).toUpperCase() + token.slice(1)
+//   );
+//   const response = capitalizedTokens.join(" ");
+//   return response;
+// });
+
+
+// Chat gpt
+
+function titleCased() {
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(' ');
+    const titleCaseWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+    return titleCaseWords.join(' ');
+  });
 }
